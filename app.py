832c4c055886a51cabe6b52746c3c73af9a228c4
aleola <alexander.olander.rosta@regionsormland.se>
2025-04-08 16:19:52 +0200

    add project

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, redirect, url_for, session, render_template
+from flask_oidc import OpenIDConnect
+from authlib.integrations.flask_client import OAuth
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+oauth = OAuth(app)
+
+oauth.register(
+    name='phx',
+    client_id='testauth',
+    client_secret='testauth',
+    server_metadata_url='https://idppreprod.regionsormland.se/testauth/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid profile email'},
+)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+@app.route('/result')
+def result():
+    user = session.get('user')
+    ssn = user.get('ssn') if user else None
+    usergivenname = user.get('usergivenname') if user else None
+    usersurname = user.get('usersurname') if user else None
+    registrationlevel = user.get('registrationlevel') if user else None
+    if registrationlevel != "PLUS" or ssn is None:
+        return redirect('/error')
+    return render_template('result.html', ssn=ssn, usergivenname=usergivenname, usersurname=usersurname, registrationlevel=registrationlevel)
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.phx.authorize_redirect(redirect_uri)
+
+@app.route('/auth')
+def auth():
+    token = oauth.phx.authorize_access_token()
+    session['user'] = token['userinfo']
+    print(token['userinfo'])  # Debugging line to check the token content
+    # Store user information in session
+    return redirect('/result')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
